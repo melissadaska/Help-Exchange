dc12d37d61a3b2d58ea2d27457196aed8fee4654
Thor40 <nicholas.kosik@gmail.com>
2020-08-19 21:02:21 -0500

    auth and typeDef created

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,52 @@
+// import gql tagged template
+const { gql } = require('apollo-server-express');
+
+// create typeDef
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        name: String
+        location: String
+        number: Int
+        requests: [Requests]
+        volunteer: [Volunteer]
+    }
+
+    type Requests {
+        _id: ID
+        title: String
+        why: String
+        createdAt: String
+        username: String
+    }
+
+    type Volunteer {
+        _id: ID
+        username: String
+        available: String
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        requests(username: String): [Requests]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addRequest(username: String!, title: String!): Requests
+        addVolunteer(userId, ID!, username: String!): User
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+`;
+
+// export typeDef
+module.exports = typeDefs;
