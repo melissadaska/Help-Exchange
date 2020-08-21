3e3f79963f08a90bc1606cada9b5e029e397f208
andytheelf <ballew22@gmail.com>
2020-08-20 19:23:15 -0500

    request help/volunteer

--- help-exchange/src/components/Header/index.js
+++ help-exchange/src/components/Header/index.js
@@ -3,8 +3,10 @@ import Navigation from '../Nav/index'
 import Navbar from 'react-bootstrap/Navbar';
 import About from '../About/index'
 import Contact from '../Contact/index'
-import Resume from '../LoginForm/index'
-import Portfolio from '../Donations/index'
+import LoginForm from '../LoginForm/index'
+import Donations from '../Donations/index'
+import Volunteer from '../LoginForm/index'
+import RequestHelp from '../Donations/index'
 
 function Header() {
 
@@ -25,8 +27,10 @@ function Header() {
                     <Route exact path="/" render={() => (<Redirect to="/about"/>)}/>
                     <Route path='/contact' component={Contact} />
                     <Route path="/about" component={About}/>
-                    <Route path='/LoginForm' component={Resume} />
-                    <Route path='/Donations' component={Portfolio} />
+                    <Route path='/LoginForm' component={LoginForm} />
+                    <Route path='/LoginForm' component={Volunteer} />
+                    <Route path='/Donations' component={RequestHelp} />
+                    <Route path='/Donations' component={Donations} />
                 </Switch>
             </div>
         </HashRouter>
