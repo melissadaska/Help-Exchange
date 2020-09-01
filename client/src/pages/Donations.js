b73dead122d9ce39b612fe2f2790251458460d43
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-08-31 19:53:50 -0500

    add stripe

--- client/src/pages/Donations.js
+++ client/src/pages/Donations.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('acct_1HMNaZAlSy38KLly');
 
 function Donations() {   
     return (
-        <div className="flex row justify-content-center">Here you can donate!</div>
-    )
+        <div className="flex row justify-content-center">
+            <button role="link">Donate Here!</button>
+        </div>
+    );
 }
 
 
