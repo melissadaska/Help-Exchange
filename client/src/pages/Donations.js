bc269bf5caf8666fcdeeca327a9615e436fd6651
andytheelf <ballew22@gmail.com>
2020-09-04 14:55:06 -0500

    donation image

--- client/src/pages/Donations.js
+++ client/src/pages/Donations.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import BalloonsImage from '../assets/Coming-Soon-Balloons-1.png';
+import '../assets/donations.scss';
 
 function Donations() {   
     return (
-        <div>Here you can donate!</div>
+        // <div>Here you can donate!</div>
+        <div className="container__card">
+        <img className="donations__card__image" src={BalloonsImage} alt="" />
+        </div>
     )
 }
 
